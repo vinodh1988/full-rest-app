da74d81d6b3afee291e03e54bf72eb778cf8f926
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-10 16:13:38 +0530

    latest

--- src/main/java/com/rest/controller/DepartmentAPI.java
+++ src/main/java/com/rest/controller/DepartmentAPI.java
@@ -3,7 +3,11 @@ package com.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +24,11 @@ public class DepartmentAPI {
 		// Logic to get departments (not implemented in this example)
 		return departmentService.getDepartments();
 	}
+	@PostMapping("")
+	public ResponseEntity<Department> addDepartment(@RequestBody Department department) {
+		// Logic to add a department (not implemented in this example)
+		departmentService.addDepartment(department);
+		return new ResponseEntity<>(department, HttpStatus.CREATED); // Return the added department for now
+	}
+	
 }
--- src/main/java/com/rest/services/DepartmentService.java
+++ src/main/java/com/rest/services/DepartmentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rest.entities.Department;
+import com.rest.entities.Employee;
 import com.rest.repository.DepartmentRepository;
 
 @Service
@@ -16,4 +17,13 @@ private DepartmentRepository departmentRepository;
 public List<Department> getDepartments() {
 	return departmentRepository.findAll();
 }
+
+public void addDepartment(Department department) {
+	// The save method of JpaRepository works for both insert and update operations.
+    for(Employee employee : department.getEmployees()) {
+    	employee.setDepartment(department); // Set the department for each employee
+		// Set the department for each employee
+	}
+	departmentRepository.save(department);
+ }
 }
